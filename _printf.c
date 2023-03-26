c7c6d45209771a4fb6daed96c791676fc4cf6137
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-03-26 19:35:30 +0300

    Fix Betty Errors

--- _printf.c
+++ _printf.c
@@ -1,49 +1,66 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <string.h>
+#include "main.h"
+
+/**
+ * _printf - printf clone
+ * @format: format specifier
+ *
+ * Return: number of characters printed
+ */
+
 int _printf(const char *format, ...)
 {
-    va_list args;
+	va_list args;
 	int i;
-    int count = 0;
-    va_start(args, format);
-    for (i = 0; format[i] != '\0'; i++) {
-        if (format[i] == '%') {
-            i++;
-            if (format[i] == 'c') {
-                char c = va_arg(args, int);
-                putchar(c);
-                count++;
-            }
-            else if (format[i] == 's') {
-                char *s = va_arg(args, char *);
-                fputs(s, stdout);
-                count += strlen(s);
-            }
-            else if (format[i] == 'd' || format[i] == 'i') {
-                int num = va_arg(args, int);
-                char str[20];
-                sprintf(str, "%d", num);
-                fputs(str, stdout);
-                count += strlen(str);
-            }
-            else if (format[i] == '%') {
-                putchar('%');
-                count++;
-            }
-            else {
-               fprintf(stderr, "Invalid conversion specifier: %%%c\n", format[i]);
-                va_end(args);
-                return -1;
-            }
-        }
-        else {
-            putchar(format[i]);
-            count++;
-        }
-    }
+	int count = 0;
 
-    va_end(args);
-    return count;
+	va_start(args, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				char c = va_arg(args, int);
+
+				putchar(c);
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				char *s = va_arg(args, char *);
+
+				fputs(s, stdout);
+				count += strlen(s);
+			}
+			else if (format[i] == 'd' || format[i] == 'i')
+			{
+				int num = va_arg(args, int);
+				char str[20];
+
+				sprintf(str, "%d", num);
+				fputs(str, stdout);
+				count += strlen(str);
+			}
+			else if (format[i] == '%')
+			{
+				putchar('%');
+				count++;
+			}
+			else
+			{
+				fprintf(stderr, "Invalid conversion specifier: %%%c\n", format[i]);
+				va_end(args);
+				return (-1);
+			}
+		}
+		else
+		{
+			putchar(format[i]);
+			count++;
+		}
+	}
+
+	va_end(args);
+	return (count);
 }
--- main.h
+++ main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
 
 int _putchar(int ch);
 int _printf(const char *format, ...);
