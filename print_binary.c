b6a1428f576cd6d322142ea45dbb4a87ee867216
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-03-27 09:56:38 +0300

    Handle negative numbers in print_binary function

--- print_binary.c
+++ print_binary.c
@@ -10,19 +10,32 @@
 int print_binary(va_list args)
 {
 	int num = va_arg(args, int);
+	int negative = 0;
 	int binary[32];
 	int j, i = 0;
 	char c;
+
+	if (num < 0)
+	{
+		negative = 1;
+		num = ~num + 1;
+	}
 
 	do {
 		binary[i++] = num % 2;
 		num /= 2;
 	} while (num > 0);
 
+	if (negative)
+	{
+		binary[i++] = 1;
+	}
+
 	for (j = i - 1; j >= 0; j--)
 	{
 		c = binary[j] + '0';
 		write(1, &c, 1);
 	}
+
 	return (i);
 }
