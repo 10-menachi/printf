4c7152f41ec25ce8a8cbfe12cac7fb34d6149950
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-03-27 08:44:15 +0300

    Fix error in print_binary

--- print_binary.c
+++ print_binary.c
@@ -11,14 +11,14 @@ int print_binary(va_list args)
 {
 	int num = va_arg(args, int);
 	int binary[32];
-	int i = 0;
+	int j, i = 0;
 
 	do {
 		binary[i++] = num % 2;
 		num /= 2;
 	} while (num > 0);
 
-	for (int j = i - 1; j >= 0; j--)
+	for (j = i - 1; j >= 0; j--)
 		putchar(binary[j] + '0');
 	return (i);
 }
