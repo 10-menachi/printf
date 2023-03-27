ec3e6d0d3a424a63e77f064f00f74aa664e057d6
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-03-27 08:31:39 +0300

    Fix write bug

--- _printf.c
+++ _printf.c
@@ -87,6 +87,12 @@ int print_string(va_list args)
 {
 	char *s = va_arg(args, char *);
 
+	if (s == NULL)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+
 	write(1, s, strlen(s));
 	return (strlen(s));
 }
--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <unistd.h>
 
 int _putchar(int ch);
 int _printf(const char *format, ...);
