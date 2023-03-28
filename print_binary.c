fcb379beab11b18a142a63076a28fe7b657e6d90
WAKIOM <agnessmanga@outlook.com>
2023-03-28 06:20:48 -0700

    handled the b specifier

--- print_binary.c
+++ print_binary.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
- * print_binary - prints an integer in binary form
- * @args: arguments list
- *
- * Return: number of characters printed
- */
-
+* print_binary - prints a binary number.
+* @args: arguments.
+* Return: 1.
+*/
 int print_binary(va_list args)
 {
 	int num = va_arg(args, int);
@@ -17,24 +15,24 @@ int print_binary(va_list args)
 
 	if (num < 0)
 	{
-		negative = 1;
-		num = ~num + 1;
+	negative = 1;
+	num = ~num + 1;
 	}
 
 	do {
-		binary[i++] = num % 2;
-		num /= 2;
+	binary[i++] = num % 2;
+	num /= 2;
 	} while (num > 0);
 
 	if (negative)
 	{
-		binary[i++] = 1;
+	binary[i++] = 1;
 	}
 
 	for (j = i - 1; j >= 0; j--)
 	{
-		c = binary[j] + '0';
-		write(1, &c, 1);
+	c = binary[j] + '0';
+	write(1, &c, 1);
 	}
 
 	return (i);
