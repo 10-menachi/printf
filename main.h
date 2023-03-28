fbd516f74320b0975c9e16fb44fd490bfb09e1ed
WAKIOM <agnessmanga@outlook.com>
2023-03-28 09:41:44 -0700

    handled flags

--- main.h
+++ main.h
@@ -8,6 +8,15 @@
 #include <unistd.h>
 #include <ctype.h>
 
+#define BUFF_SIZE 1024
+
+/* flags */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list args);
@@ -15,6 +24,7 @@ int print_string(va_list args);
 int print_int(va_list args);
 int print_argument(char specifier, va_list args);
 int print_reverse(va_list args);
+int get_flags(const char *format, int *i);
 int print_binary(va_list args);
 int printrot13string(va_list args);
 int print_pointer(va_list args);
